e64d74f37c60e0635e57c093e5d2a0186296c91d
Yan Tuzlukov <tyzluckoff@yandex.ru>
2022-07-04 03:03:57 +0700

    Initial commit

---
+++ src/main/java/pro/sky/storedemoproj/StoreDemoProjApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.storedemoproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreDemoProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreDemoProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/storedemoproj/StoreDemoProjApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.storedemoproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreDemoProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
